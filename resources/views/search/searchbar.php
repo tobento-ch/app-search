60bf79909ab927e3ffb980c1c28c077e23259788
TOBENTOch <info@tobento.ch>
2025-05-27 18:42:59 +0200

    placeholder added

--- resources/views/search/searchbar.php
+++ resources/views/search/searchbar.php
@@ -21,6 +21,8 @@ $form = $view->form();
                 'autocorrect' => 'off',
                 'spellcheck' => 'false',
                 'aria-label' => $view->trans('Search'),
+                'placeholder' => $view->trans($inputPlaceholder),
+                'class' => 'small',
             ],
             withInput: true,
         ) ?>
@@ -58,14 +60,15 @@ $form = $view->form();
                         'autocorrect' => 'off',
                         'spellcheck' => 'false',
                         'aria-label' => $view->trans('Search'),
+                        'placeholder' => $view->trans($inputPlaceholder),
                     ],
                     withInput: true,
                 ) ?>
                 <button class="icon-right link icon px-s py-xs" aria-label="<?= $view->etrans('Search') ?>"><svg  xmlns="http://www.w3.org/2000/svg"  width="24"  height="24"  viewBox="0 0 24 24"  fill="none"  stroke="currentColor"  stroke-width="2"  stroke-linecap="round"  stroke-linejoin="round"  class="icon icon-tabler icons-tabler-outline icon-tabler-search"><path stroke="none" d="M0 0h24v24H0z" fill="none"/><path d="M10 10m-7 0a7 7 0 1 0 14 0a7 7 0 1 0 -14 0" /><path d="M21 21l-6 -6" /></svg></button>
             </div>
             <?= $form->close() ?>
-            <div class="modal-foot"><span class="link modal-close"><?= $view->etrans('close') ?></span></div>
         </div>
         <div class="modal-body" data-searchbar-results="modal"></div>
+        <div class="modal-foot"><span class="link modal-close"><?= $view->etrans('close') ?></span></div>
     </div>
 </div>
